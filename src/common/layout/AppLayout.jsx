69f4c187d52bfe16d93041d41d3d60178086cca7
Bibekananda Mahata <bibekmahata112@gmail.com>
2025-12-09 19:52:20 +0530

    merge

--- src/common/layout/AppLayout.jsx
+++ src/common/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ function AppLayout({ children }) {
     return (
         <div className='w-full min-h-screen flex flex-col'>
             <Navbar />
-            <main className='w-screen grow h-full overflow-y-auto'>{children}</main>
+            <main className='w-full grow h-full overflow-y-auto'>{children}</main>
         </div>
     )
 }
