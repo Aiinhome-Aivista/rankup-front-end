27b2ab58ba7c59fb03d059af2736a69b5c7dc9ee
Bibekananda Mahata <bibekmahata112@gmail.com>
2025-12-09 18:13:05 +0530

    homepage section1

--- src/pages/common/homepage/Homepage.jsx
+++ src/pages/common/homepage/Homepage.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import '../../../style/homepage.css'
-import codingImage from '../../../assets/coding-a-website.svg'
+import TopSection from './ui/TopSection.jsx'
 
 function Homepage() {
   return (
     <div className='flex flex-col w-full h-full'>
       <section className='bg-[#514CF133] flex flex-col'>
-        <div className='flex justify-between items-center'>
-          <img src={codingImage} alt="coding" />
-          <div className="flex flex-col">
-            <h1>Nest Generation Assessment Platform For Every School</h1>
-            <p>Empower educator, engage students, and improve learning outcomes</p>
-          </div>
-          <div className="flex flexx-col">
-            <img src="" alt="" />
-            <img src="" alt="" />
-          </div>
-        </div>
+        <TopSection/>
       </section>
-      <section></section>
+      <section>
+        <MiddleSection/>
+      </section>
       <footer></footer>
     </div>
   )
