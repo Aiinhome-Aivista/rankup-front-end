d3f342374ccc9dee07e98c9251872d32b1670ac3
Rohan Das <rohandas8944@gmail.com>
2025-12-10 16:56:41 +0530

    add

--- src/pages/common/homepage/ui/TopSection.jsx
+++ src/pages/common/homepage/ui/TopSection.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import codingImage from "../../../../assets/coding-a-website.svg";
 import studying from "../../../../assets/student-studying.svg";
 import fun from "../../../../assets/having-fun.svg";
+import vector2 from "../../../../assets/Vector 2.svg";
+import vector3 from "../../../../assets/Vector 3.svg";
+import vector4 from "../../../../assets/Vector 4.svg";
+import vector5 from "../../../../assets/Vector 5.svg";
 import WaveBackground from "./WaveBackground";
-import SchoolIcon from '@mui/icons-material/School';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import PublicIcon from '@mui/icons-material/Public';
-import SpaIcon from '@mui/icons-material/Spa';
+import SchoolIcon from "@mui/icons-material/School";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import PublicIcon from "@mui/icons-material/Public";
+import SpaIcon from "@mui/icons-material/Spa";
 
 function TopSection() {
   return (
     <div className="relative w-full min-h-[140vh] bg-gradient-to-b from-purple-50 to-blue-50 overflow-hidden font-sans">
       {/* Main Content Container */}
       <div className="container mx-auto pt-32 pb-60 text-center relative z-10">
-
         {/* Typography */}
         <h1 className="text-5xl md:text-6xl font-bold text-indigo-600 mb-4 max-w-4xl mx-auto leading-tight">
           Nest Generation Assessment <br /> Platform For Every School
@@ -25,24 +28,46 @@ function TopSection() {
         {/* Floating Illustrations */}
         {/* Coding Girl - Left */}
         <div className="absolute left-10 top-32 hidden lg:block w-64 opacity-90 hover:scale-105 transition-transform duration-500">
-          <img src={codingImage} alt="Design and code" className="w-full h-auto drop-shadow-lg" />
+          <img
+            src={codingImage}
+            alt="Design and code"
+            className="w-full h-auto drop-shadow-lg"
+          />
         </div>
 
         {/* Student Studying - Top Right */}
-        <div className="absolute right-10 top-10 hidden lg:block w-72 opacity-90 hover:scale-105 transition-transform duration-500">
-          <img src={studying} alt="Student studying" className="w-full h-auto drop-shadow-lg" />
+        <div className="absolute right-10 top-10 hidden lg:block w-72 opacity-90 hover:scale-105 transition-transform duration-500 z-1">
+          <img
+            src={studying}
+            alt="Student studying"
+            className="w-full h-auto drop-shadow-lg"
+          />
         </div>
 
         {/* Having Fun - Mid Right (Lower) */}
-        <div className="absolute right-20 top-96 hidden lg:block w-64 opacity-90 hover:scale-105 transition-transform duration-500">
-          <img src={fun} alt="Students having fun" className="w-full h-auto drop-shadow-lg" />
+        <div className="absolute right-20 top-96 hidden lg:block w-64 opacity-90 hover:scale-105 transition-transform duration-500 z-1">
+          <img
+            src={fun}
+            alt="Students having fun"
+            className="w-full h-auto drop-shadow-lg"
+          />
         </div>
       </div>
 
       {/* Wave Background */}
-      <div className="absolute bottom-0 left-0 w-full z-0">
+      <div className="absolute top-1/2 left-0 w-full z-5">
         <WaveBackground />
+        {/* <img src={vector5} alt="" className=" w-full animate-wave-slow" /> */}
       </div>
+      {/* <div className="absolute top-50 -left-40 w-full z-3">
+        <img src={vector3} alt="" className=" w-full animate-wave-medium" />
+      </div>
+      <div className="absolute top-80 left-0 w-full z-4">
+        <img src={vector2} alt="" className=" w-full animate-wave-fast" />
+      </div>
+      <div className="absolute top-90 left-45 w-full z-4">
+        <img src={vector4} alt="" className=" w-full animate-wave-slow" />
+      </div> */}
     </div>
   );
 }
