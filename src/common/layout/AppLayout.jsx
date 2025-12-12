84d8bb7523bd1dfa884f63a26288912ca5c1ff78
Bibekananda Mahata <bibekmahata112@gmail.com>
2025-12-12 14:50:47 +0530

    parent dashboard

--- src/common/layout/AppLayout.jsx
+++ src/common/layout/AppLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 function AppLayout({ children }) {
     return (
-        <div className='w-full min-h-screen flex flex-col'>
-            <Navbar />
-            <main className='w-full grow h-full overflow-y-auto'>{children}</main>
+        <div className='w-full min-h-screen bg-[#F3F4FF]'>
+            <Sidebar />
+            <div className='flex flex-col min-h-screen'>
+                <Navbar />
+                <main className='w-full flex-1 overflow-y-auto'>{children}</main>
+            </div>
         </div>
     )
 }
