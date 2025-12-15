d663b39ee3e4d3756165ec820bbbe9f796526517
Bibekananda Mahata <bibekmahata112@gmail.com>
2025-12-15 19:39:11 +0530

    user auth added

--- src/common/layout/AppLayout.jsx
+++ src/common/layout/AppLayout.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import React, { useContext } from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import { AuthContext } from "../../context/AuthContext";
+import Header from "./Header";
 
 function AppLayout({ children }) {
-    return (
-        <div className='w-full min-h-screen bg-[#F3F4FF]'>
-            <div className='flex flex-col min-h-screen'>
-                <Navbar />
-                <main className='w-full flex-1 overflow-y-auto'>{children}</main>
-            </div>
-        </div>
-    )
+  const { isLoggedIn } = useContext(AuthContext);
+  return (
+    <div className="w-full min-h-screen bg-[#F3F4FF]">
+      <div className="flex flex-col min-h-screen">
+        {isLoggedIn ? <Header /> : <Navbar />}
+        {isLoggedIn && <Sidebar />}
+        <main className="w-full flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
