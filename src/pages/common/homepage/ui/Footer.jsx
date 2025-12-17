33aae2ffde91e821d7c9a2fa4084348649afacf2
Rohan Das <rohandas8944@gmail.com>
2025-12-17 12:17:38 +0530

    add

--- src/pages/common/homepage/ui/Footer.jsx
+++ src/pages/common/homepage/ui/Footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import blackLogo from "../../../../assets/icons/black-logo-main.svg";
+import rankUpAcademy from "../../../../assets/icons/Rank Up Academy.svg";
+import footerImg from "../../../../assets/icons/footer-img.svg";
 
 function Footer() {
   return (
@@ -6,13 +9,13 @@ function Footer() {
       <div className="max-w-6xl mx-auto px-10 lg:px-14">
         {/* Logo Section */}
         <div className="flex items-center gap-3 mb-10">
-          <img 
-            src="/black-logo-main.svg" 
-            alt="Rank Up Academy Logo" 
+          <img
+            src={blackLogo}
+            alt="Rank Up Academy Logo"
             className="h-8 w-auto"
           />
           <div className="flex items-center">
-            <img src="/Rank Up Academy.svg" alt="" className="h-5 w-auto" />
+            <img src={rankUpAcademy} alt="" className="h-5 w-auto" />
           </div>
         </div>
 
@@ -20,25 +23,47 @@ function Footer() {
         <div className="flex flex-col md:flex-row w-full justify-between gap-12 mb-20">
           {/* Column 1 */}
           <div className="flex flex-col space-y-4">
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">About Us</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Features</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Pricing</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Documentation</a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              About Us
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Features
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Pricing
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Documentation
+            </a>
           </div>
 
           {/* Column 2 */}
           <div className="flex flex-col space-y-4">
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Support</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Terms & Conditions</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Privacy Policy</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Contact</a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Support
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Terms & Conditions
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Privacy Policy
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Contact
+            </a>
           </div>
 
           {/* Column 3 */}
           <div className="flex flex-col space-y-4">
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Contact</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">FAQ</a>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">Blog</a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Contact
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              FAQ
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 text-sm">
+              Blog
+            </a>
           </div>
         </div>
 
@@ -51,9 +76,9 @@ function Footer() {
       </div>
 
       {/* Decorative Image */}
-      <img 
-        src="/footer-img.svg" 
-        alt="Footer Decoration" 
+      <img
+        src={footerImg}
+        alt="Footer Decoration"
         className="absolute bottom-0 left-0 w-24 md:w-32 lg:w-38 z-10"
       />
     </footer>
