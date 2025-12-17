90a8b74575586eb19b38254fc3eb339dbec58659
Rohan Das <rohandas8944@gmail.com>
2025-12-18 02:20:13 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import "primereact/resources/primereact.min.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import App from "./App.jsx";
 
+// Disable browser scroll restoration BEFORE React initializes
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
