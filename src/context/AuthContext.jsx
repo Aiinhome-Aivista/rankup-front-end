65fdfd99145d3f22944bf4206e2510aeaab2fdbc
Rohan Das <rohandas8944@gmail.com>
2025-12-18 02:41:07 +0530

    add

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <AuthContext.Provider
       value={{
