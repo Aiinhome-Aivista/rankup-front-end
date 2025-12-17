1d459379d3b562af0ec8f89714b47db7c91dfa1d
Arendu Chanda <arenduchanda1997@gmail.com>
2025-12-17 13:55:28 +0530

    added

--- src/pages/common/homepage/Homepage.jsx
+++ src/pages/common/homepage/Homepage.jsx
@@ -3,19 +3,12 @@ import "../../../style/homepage.css";
 import TopSection from "./ui/TopSection.jsx";
 import MiddleSection from "./ui/MiddleSection.jsx";
 import Footer from "./ui/Footer.jsx";
+import ParallaxLanding from "./parallax/parallazView.jsx";
 
 function Homepage() {
   return (
     <div className="flex flex-col w-full h-full">
-      <section className="bg-[#514CF133]">
-        <TopSection />
-      </section>
-      <section>
-        <MiddleSection />
-      </section>
-      <footer>
-        <Footer />
-      </footer>
+    <ParallaxLanding />
     </div>
   );
 }
---
+++ src/pages/common/homepage/parallax/parallazView.jsx
@@ -0,0 +1,54 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+
+export default function ParallaxLanding() {
+  const { scrollYProgress } = useScroll();
+
+  const layer1 = useTransform(scrollYProgress, [0, 1], [0, -40]);
+  const layer2 = useTransform(scrollYProgress, [0, 1], [0, -80]);
+  const layer3 = useTransform(scrollYProgress, [0, 1], [0, 400]);
+  const layer4 = useTransform(scrollYProgress, [0, 1], [0, -160]);
+  const layer5 = useTransform(scrollYProgress, [0, 1], [0, -600]);
+
+  return (
+    <div className="relative h-[160vh] overflow-hidden bg-black">
+      {/* BACK WAVES */}
+      <motion.div
+        style={{ y: layer1 }}
+        className="absolute top-[55vh] bottom-0 left-0 right-0 z-10 bg-[url('/Parallax/Layer01.svg')] bg-no-repeat bg-bottom "
+      />
+
+      <motion.div
+        style={{ y: layer2 }}
+        className="absolute top-[55vh] bottom-0 left-0 right-0 z-20 bg-[url('/Parallax/Layer02.svg')] bg-no-repeat bg-bottom "
+      />
+
+      {/* TEXT */}
+      <motion.div
+        style={{ y: layer3 }}
+        className="absolute top-[30vh] bottom-0 left-0 right-0 z-30 flex items-start justify-center pt-16"
+      >
+        <h1 className="text-white text-6xl font-bold drop-shadow-xl">
+          Parallax Landing Page
+        </h1>
+      </motion.div>
+
+      {/* FRONT WAVES */}
+      <motion.div
+        style={{ y: layer4 }}
+        className="absolute top-[55vh] bottom-0 left-0 right-0 z-40 bg-[url('/Parallax/Layer03.svg')] bg-no-repeat bg-bottom "
+      />
+
+      <motion.div
+        style={{ y: layer5 }}
+        className="absolute top-[55vh] bottom-0 left-0 right-0 z-50 bg-[url('/Parallax/Layer04.svg')] bg-no-repeat bg-bottom "
+      />
+
+      {/* SCROLL CONTENT */}
+      <div className="relative z-50 mt-[120vh] text-center text-white">
+        <p className="text-xl max-w-xl mx-auto">
+          Parallax waves start from 60% viewport height.
+        </p>
+      </div>
+    </div>
+  );
+}
